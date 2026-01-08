5f3657d229dc7a31c00c6e3389bad507970a2975
sarangabani1984 <sarangabani1984@gmail.com>
2026-01-08 20:35:02 +0530

    second highest salary using TOP and ORDER BY

--- 2n_highest_salary.sql
+++ 2n_highest_salary.sql
@@ -1,15 +1,11 @@
--- Query to find the second highest salary using ROW_NUMBER()
-SELECT * 
-FROM (
-    SELECT *, ROW_NUMBER() OVER (ORDER BY salary DESC) AS rnum 
-    FROM Employee 
-) AS ranked_employees
-WHERE rnum = 2;
 
--- Query to find the second highest salary using MAX and subquery
-SELECT MAX(salary) 
-FROM Employee 
-WHERE salary < (
-    SELECT MAX(salary) 
+
+-- Query to find the second highest salary using TOP and ORDER BY
+SELECT TOP 1 salary
+FROM (
+    SELECT TOP 2 salary
     FROM Employee
-);
+    ORDER BY salary DESC
+) AS temp
+ORDER BY salary ASC;
+
