58c3d1697a1ff00ab4f3514a6aa06ad772d5ab23
sarangabani1984 <sarangabani1984@gmail.com>
2026-01-13 20:41:24 +0530

    Left_anti_join

--- join_handson.sql
+++ join_handson.sql
@@ -1,5 +1,22 @@
+SELECT 
+    c.CustomerID,
+    c.CustomerName,
+    c.City,
+    c.Country
+FROM Customers c
+LEFT JOIN Orders o ON c.CustomerID = o.CustomerID
+WHERE o.OrderID IS NULL;
 
-select C.*, O.OrderID, O.OrderDate
-from customers C
-INNER JOIN orders O
-ON C.CustomerID = O.CustomerID ;
+SELECT 
+    c.CustomerID,
+    c.CustomerName,
+    o.OrderID,
+    o.CustomerID AS OrderCustomerID,
+    o.OrderDate,
+    o.TotalAmount
+FROM Customers c
+FULL OUTER JOIN Orders o ON c.CustomerID = o.CustomerID;
+
+
+
+
