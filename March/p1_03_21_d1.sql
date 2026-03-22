8d3655345e283263c3b3ecac101b2f0d07457224
sarangabani1984 <sarangabani1984@gmail.com>
2026-03-22 19:17:44 +0530

    SQL Interviwe Questin

--- March/p1_03_21_d1.sql
+++ March/p1_03_21_d1.sql
@@ -1,7 +1,31 @@
 use sarang;
 
--- Check table structure (SQL Server syntax)
 EXEC sp_help 'Employees';
 
--- Find duplicates by FirstName
-SELECT FirstName, COUNT(*) AS DuplicateCount FROM Employees GROUP BY FirstName HAVING COUNT(*) > 1;
+SELECT * from Employees;
+
+-- SELECT 
+-- FirstName,
+-- LastName,
+-- Email,
+-- Department, 
+-- COUNT(*) AS DuplicateCount 
+-- FROM Employees GROUP BY FirstName, LastName, Email, Department HAVING COUNT(*) > 1;
+
+
+with dupilcate as (
+    SELECT 
+        *,
+        COUNT(*) OVER (PARTITION BY FirstName, LastName, Email, Department) AS DuplicateCount
+    FROM Employees
+)
+SELECT *
+FROM dupilcate
+WHERE DuplicateCount > 1
+
+
+
+    SELECT 
+        *,
+        ROW_NUMBER() OVER (PARTITION BY FirstName, LastName, Email ORDER BY EmployeeID) AS RowNum
+    FROM Employees
